abaf5fcc57f1da98d39a5c82941a8effb53e50db
Lakshan <75500772+M-lakshan@users.noreply.github.com>
2023-01-05 20:22:37 +0530

    Update CashRegister.js
    100% working script.

--- CashRegister.js
+++ CashRegister.js
@@ -1,6 +1,9 @@
-function checkCashRegister(price, cash, cid) {
+function checkCashRegister(prc, csh, cid) {
+    let price = parseFloat(Math.round(prc*Math.pow(10,2))/Math.pow(10,2));
+    let cash = parseFloat(Math.round(csh*Math.pow(10,2))/Math.pow(10,2));
     let current_value = 0;
-    let change = amount_determine(parseFloat(price),parseFloat(cash));
+    let change = amount_determine(cash,price);
+    let moneyDrawer_arr = [];
     const return_sts = { "status": "", "change": [] };
     const moneyDrawer = {
         "PENNY": 0, "NICKEL": 0,
@@ -19,138 +22,171 @@ function checkCashRegister(price, cash, cid) {
         moneyDrawer[type] = value;
     });
 
+    function moneyDrawer_keyNames(name) {
+        switch(name) { //obj_key_names
+            case "PENNY": return [0.01,"PENNY"];
+            case "NICKEL": return [0.05,"PENNY"];
+            case "DIME": return [0.1,"NICKEL"];
+            case "QUARTER": return [0.25,"DIME"];
+            case "ONE": return [1,"QUARTER"];
+            case "FIVE": return [5,"ONE"];
+            case "TEN": return [10,"FIVE"];
+            case "TWENTY": return [20,"TEN"];
+            case "ONE HUNDRED": return [100,"TWENTY"];
+        }
+    }
+
     function status_confirm(sts) {
         switch(sts) {
-            case "OPEN":
+            case "OPEN": 
                 return_sts["status"] = "OPEN";
+            break;
+            case "CLOSED":
+                return_sts["status"] = "CLOSED";
             break;
             case "INSUFFICIENT_FUNDS":
                 return_sts["status"] = "INSUFFICIENT_FUNDS";
                 return_sts["change"] = [];
             break;
-            case "CLOSED":
-                return_sts["status"] = "CLOSED";
-                return_sts["change"] = [["PENNY", 0],["NICKEL", 0],["DIME", 0],["QUARTER", 0],["ONE", 0],
-                                        ["FIVE", 0],["TEN", 0],["TWENTY", 0],["ONE HUNDRED", 0]];
-            break;
-            case "sts_pending":
-
-            break;
         }
     }
 
-    function amount_determine(promt,intake) {
-        if(intake-promt < 0.05) { return "PENNY"; }
-        else if(intake-promt < 0.1 && intake-promt >= 0.05) { return "NICKEL"; }
-        else if(intake-promt < 0.25 && intake-promt >= 0.1) { return "DIME"; }
-        else if(intake-promt < 1 && intake-promt >= 0.25) { return "QUARTER"; }
-        else if(intake-promt < 5 && intake-promt >= 1) { return "ONE"; }
-        else if(intake-promt < 10 && intake-promt >= 5) { return "FIVE"; }
-        else if(intake-promt < 20 && intake-promt >= 10) { return "TEN"; }
-        else if(intake-promt < 100 && intake-promt >= 20) { return "TWENTY"; }
-        else if(intake-promt >= 100) { return "ONE HUNDRED"; }
+    function amount_determine(intake,promt) {
+        let diff_amt = Math.round((intake-promt)*Math.pow(10,2))/Math.pow(10,2);
+        if(diff_amt < 0.05) { return "PENNY"; }
+        else if(diff_amt < 0.1 && diff_amt >= 0.05) { return "NICKEL"; }
+        else if(diff_amt < 0.25 && diff_amt >= 0.1) { return "DIME"; }
+        else if(diff_amt < 1 && diff_amt >= 0.25) { return "QUARTER"; }
+        else if(diff_amt < 5 && diff_amt >= 1) { return "ONE"; }
+        else if(diff_amt < 10 && diff_amt >= 5) { return "FIVE"; }
+        else if(diff_amt < 20 && diff_amt >= 10) { return "TEN"; }
+        else if(diff_amt < 100 && diff_amt >= 20) { return "TWENTY"; }
+        else if(diff_amt >= 100) { return "ONE HUNDRED"; }
     }
+    
+    function amount_validate(ctg,intake_alt,promt_alt) {
+        let balance_sts = true;
+        let recurrent_index = 8;
+        let backup_from = [];
+        let difference_amount = intake_alt-promt_alt;
 
-    function amount_generate(intake,promt,main_ctg) {
+        switch(ctg) { //obj_key_name
+            case "PENNY": recurrent_index=0; break;
+            case "NICKEL": recurrent_index=1; break;
+            case "DIME": recurrent_index=2; break;
+            case "QUARTER": recurrent_index=3; break;
+            case "ONE": recurrent_index=4; break;
+            case "FIVE": recurrent_index=5; break;
+            case "TEN": recurrent_index=6; break;
+            case "TWENTY": recurrent_index=7; break;
+            case "ONE HUNDRED": recurrent_index=8; break;
+        }
+
+        for(let i=0; i < Object.keys(moneyDrawer).length; i++) {
+            moneyDrawer_arr.push([
+                (Object.keys(moneyDrawer)[i]),
+                moneyDrawer_keyNames((Object.keys(moneyDrawer)[i]))[0],
+                parseFloat(moneyDrawer[Object.keys(moneyDrawer)[i]]),
+                Math.round(
+                    (parseFloat(moneyDrawer[Object.keys(moneyDrawer)[i]]) / moneyDrawer_keyNames((Object.keys(moneyDrawer)[i]))[0])
+                    * Math.pow(10,2)
+                ) / Math.pow(10,2)
+            ]);
+        }
+
+        while(balance_sts && recurrent_index >= 0) {
+            if(difference_amount >= moneyDrawer_arr[recurrent_index][2]) {
+                if(moneyDrawer_arr[recurrent_index][2] != 0) {
+                    difference_amount = Math.round(
+                                            (difference_amount - moneyDrawer_arr[recurrent_index][2]) * Math.pow(10,2)
+                                        ) / Math.pow(10,2);
+                    backup_from.push([moneyDrawer_arr[recurrent_index][0],moneyDrawer_arr[recurrent_index][3]]);
+                }
+                balance_sts = true;
+            } else {
+                let count = 1;
+                while((count * moneyDrawer_arr[recurrent_index][1]) <= difference_amount) { count++; }
+                if((count * moneyDrawer_arr[recurrent_index][1]) > difference_amount) { count-- }
+                if(count != 0) {
+                    difference_amount = Math.round(
+                                            (difference_amount - moneyDrawer_arr[recurrent_index][1] * count)
+                                        * Math.pow(10,2))/Math.pow(10,2)
+                    backup_from.push([moneyDrawer_arr[recurrent_index][0],count]);
+                }
+                balance_sts = true;
+            }
+
+            if(recurrent_index == 0 && difference_amount == 0 && backup_from.length == 0) {
+                balance_sts = false;
+            }
+
+            recurrent_index--;
+        }
+
+        return (backup_from.length != 0 && difference_amount == 0) ? [true,backup_from] : [false,""];
+    }
+    
+    function amount_calculate(result_arr) {
         let count = 0;
-        let compare_val = intake-promt;
-        let ctg_calc = ""
+        let recurring_val = 0;
+        let recurring_arr = 0;
+        let moneyDrawer_obj_copy = Object.assign({}, moneyDrawer);
+        let status = '';
         current_value = 0;
 
-        if(intake!=promt) {
-            switch(main_ctg) {
-                case "PENNY": ctg_calc = amount_calculate(0.01,0.01,0.01,"PENNY","PENNY"); break;
-                case "NICKEL": ctg_calc = amount_calculate(0.05,0.01,0.01,"PENNY","PENNY"); break;
-                case "DIME": ctg_calc = amount_calculate(0.1,0.05,0.01,"NICKLE","PENNY"); break;
-                case "QUARTER": ctg_calc = amount_calculate(0.25,0.1,0.05,"DIME","NICKLE"); break;
-                case "ONE": ctg_calc = amount_calculate(1,0.25,0.1,"QUARTER","DIME"); break;
-                case "FIVE": ctg_calc = amount_calculate(5,1,0.25,"ONE","QUARTER"); break;
-                case "TEN": ctg_calc = amount_calculate(10,5,1,"FIVE","ONE"); break;
-                case "TWENTY": ctg_calc = amount_calculate(20,10,5,"TEN","FIVE"); break;
-                case "ONE HUNDRED": ctg_calc = amount_calculate(100,20,10,"TWENTY","TEN"); break;
-            }
+        while(recurring_arr < result_arr.length) {
+            recurring_val = moneyDrawer_keyNames(result_arr[recurring_arr][0])[0] * result_arr[recurring_arr][1];
+            current_value = Math.round((current_value + recurring_val) * Math.pow(10,2)) / Math.pow(10,2);
             
-            switch(ctg_calc) {
-                case "ctg_val_alt_i_redirect": case "ctg_val_alt_ii_redirect":
-                    return amount_generate(compare_val,current_value,amount_determine(current_value,compare_val));
-                case "funds_insuficient": status_confirm("INSUFFICIENT_FUNDS"); break;
-                case "funds_transfered": status_confirm("OPEN"); break;
-            }
-        } else {
-            status_confirm("OPEN");
+            return_sts["change"].push([result_arr[recurring_arr][0],recurring_val]);
+
+            moneyDrawer_obj_copy[result_arr[recurring_arr][0]] = Math.round((moneyDrawer_obj_copy[result_arr[recurring_arr][0]] - recurring_val) * Math.pow(10,2)) / Math.pow(10,2);
+            recurring_arr++;
         }
 
-        function amount_calculate(ctg_val,ctg_val_alt_i,ctg_val_alt_ii,ctg_val_alt_i_n,ctg_val_alt_ii_n) {
-            if(compare_val < moneyDrawer[main_ctg]*ctg_val) {
-                while(count<parseInt(compare_val/ctg_val)) { current_value+=ctg_val; moneyDrawer[main_ctg]--; count++; }
-                return_sts["change"].push([main_ctg,current_value]);
+        for(let i=0; i < Object.keys(moneyDrawer_obj_copy).length; i++) {
+            if(moneyDrawer_obj_copy[(Object.keys(moneyDrawer_obj_copy)[i])]==0) { count++; }
+        }
 
-                if((compare_val)!=current_value) {
-                    if(moneyDrawer[ctg_val_alt_i_n]*ctg_val_alt_i - (compare_val-current_value)!=0) {
-                        return "ctg_val_alt_i_redirect";
-                    } else if(moneyDrawer[ctg_val_alt_ii_n]*ctg_val_alt_ii - ((intake-promt)-current_value)!=0) {
-                        return "ctg_val_alt_ii_redirect";
-                    } else {
-                        return "funds_insuficient";
-                    }
-                } else { return "funds_transfered"; }
-            } else if(compare_val == moneyDrawer[main_ctg]*ctg_val) {
-                while(current_value<parseFloat(compare_val)) { current_value+=ctg_val; }
-                return_sts["change"].push([main_ctg,current_value]);
-                return "funds_transfered";
-            } else { return "funds_insuficient"; }
-        }        
+        if(count == Object.keys(moneyDrawer).length) {
+            return_sts["change"] = [];
+            for(let i=0; i < Object.keys(moneyDrawer).length; i++) {
+                return_sts["change"].push([
+                    (Object.keys(moneyDrawer)[i]),
+                    parseFloat(moneyDrawer[Object.keys(moneyDrawer)[i]])
+                ]);
+            }
+            status = "CLOSED";
+        } else {
+            status = "OPEN";
+        }
+
+        return status_confirm(status);
     }
 
-    switch(change) {
-        case "PENNY":
-            if(moneyDrawer["PENNY"]*0.01 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"PENNY");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "NICKEL":
-            if(moneyDrawer["NICKEL"]*0.05 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"NICKEL");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "DIME":
-            if(moneyDrawer["DIME"]*0.1 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"DIME");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "QUARTER":
-            if(moneyDrawer["QUARTER"]*0.25 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"QUARTER");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "ONE":
-            if(moneyDrawer["ONE"] - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"ONE");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "FIVE":
-            if(moneyDrawer["FIVE"]*5 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"FIVE");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "TEN":
-            if(moneyDrawer["TEN"]*10 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"TEN");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "TWENTY":
-            if(moneyDrawer["TWENTY"]*20 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"TWENTY");
-            } else { status_confirm("sts_pending"); }
-        break;
-        case "ONE HUNDRED":
-            if(moneyDrawer["ONE HUNDRED"]*100 - (parseFloat(cash)-parseFloat(price))!=0) {
-                amount_generate(parseFloat(cash),parseFloat(price),"ONE HUNDRED");
-            } else { status_confirm("sts_pending"); }
-        break;
-    }
+    let amt_validate = amount_validate(change,cash,price);
+
+    if(amt_validate[0]) {
+        amount_calculate(amt_validate[1]);
+    } else { status_confirm("INSUFFICIENT_FUNDS"); }
 
     return return_sts;
 }
+  
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+
+// should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+// should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
